7f5617fe3a6063130b8b714b53a1abda2811d0b3
Kiyoshi <kiyoshibudokan@hotmail.com>
2024-02-12 19:28:14 -0300

    Adjusting the react multi carousel

--- src/components/Product/ProductTileHome/index.js
+++ src/components/Product/ProductTileHome/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ProductTileDescription from "../ProductTile/components/ProductTileDescription";
 import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import ButtonHoverEffect from "../../Button/ButtonHoverEffect";
 import ProductExpirationTime from "../ProductExpirationTime";
 
